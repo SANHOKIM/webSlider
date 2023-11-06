283091a1f59d2f4208695a8d810cd87bd613698e
SANHOKIM <awrko@naver.com>
2023-11-06 13:48:48 +0900

    B 상단 중단 하단 반복연습함

--- js/web.js
+++ js/web.js
@@ -1,6 +1,6 @@
+/* 
 
-
-$(document).ready(function(){
+$(document).ready(function () {
     // 화면이 준비되면 실행해라.
     // setInterval, animate, css, appendTo(객체 위치를 바꾸는것)
 
@@ -21,25 +21,42 @@ $(document).ready(function(){
     // appendTo 프로토 타입 만드세요 
     // $(옮길태그).appendTo($(부모가될태그))
 
-// const aniw = $(".swiper-slide").width(); height 해보려고 주석함
-// const aniw = $(".swiper-slide").height();  좌우 상하 다음 세번째꺼 하려고 주석함
+    // const aniw = $(".swiper-slide").width(); height 해보려고 주석함
+    // const aniw = $(".swiper-slide").height();  좌우 상하 다음 세번째꺼 하려고 주석함
 
-// const aniw = $(".swiper-slide").css("width"); jquery는 규칙이 없으니 위에처럼도 쓰이는거다. 값을 넣으면 그 값이 들어가고 값이 없으면 가져오는 것. "width"를 가져오는 것.
+    // const aniw = $(".swiper-slide").css("width"); jquery는 규칙이 없으니 위에처럼도 쓰이는거다. 값을 넣으면 그 값이 들어가고 값이 없으면 가져오는 것. "width"를 가져오는 것.
 
-// 전자는 숫자 후자는 문자로 받아요. (css("width")는 문자로 받아 짜증난다던데 이유는 샘께 한번더 물어보기 )
+    // 전자는 숫자 후자는 문자로 받아요. (css("width")는 문자로 받아 짜증난다던데 이유는 샘께 한번더 물어보기 )
 
-let count = 0;
+    let count = 0;
 
-    setInterval(function() {  
+    setInterval(function () {
 
-//         $(".swiper-wrapper").animate({"marginTop" : -aniw } , 400, function () {  좌우 상하 다음 세번째꺼 하려고 주석함
-// $ (".swiper-slide").eq(0).appendTo($(".swiper-wrapper")) 좌우 상하 다음 세번째꺼 하려고 주석함
-//             $(".swiper-wrapper").css("marginTop", 0) 좌우 상하 다음 세번째꺼 하려고 주석함
-//         })               좌우 상하 다음 세번째꺼 하려고 주석함
+        //         $(".swiper-wrapper").animate({"marginTop" : -aniw } , 400, function () {  좌우 상하 다음 세번째꺼 하려고 주석함
+        // $ (".swiper-slide").eq(0).appendTo($(".swiper-wrapper")) 좌우 상하 다음 세번째꺼 하려고 주석함
+        //             $(".swiper-wrapper").css("marginTop", 0) 좌우 상하 다음 세번째꺼 하려고 주석함
+        //         })               좌우 상하 다음 세번째꺼 하려고 주석함
         count++; // 순환 코드는 몫을 구한 후 남은 나머지 식에서 나온 것이예요.
         count = count % 3;
         $(".swiper-slide").eq(count).addClass("on").siblings().removeClass("on")
-    },3000)
+    }, 3000)
 
 })
 
+ */
+// ---------------------
+
+$(document).ready(function () {
+
+    let count = 0;
+
+    setInterval(fadeani, 3000)
+
+    function fadeani() { //선언적함수
+        count++;
+        count = count % $(".swiper-slide").length;
+        // $(".swiper-slide").removeClass("on")
+        $(".swiper-slide").eq(count).addClass("on").siblings().removeClass("on")
+    }
+
+})
